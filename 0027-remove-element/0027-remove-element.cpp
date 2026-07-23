c6350c56c9c67858aedb56d8afa42b68ba2724f1
Sarthak Agarwal <163162441+sarthakagarwalco@users.noreply.github.com>
2026-07-24 00:06:51 +0530

    Time: 0 ms (100%), Space: 11.8 MB (12.76%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,6 +1,17 @@
+#include <vector>
+
 class Solution {
 public:
-    void solveSudoku(vector<vector<char>>& board) {
+    int removeElement(std::vector<int>& nums, int val) {
+        int k = 0; // Pointer for the next valid element's position
         
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != val) {
+                nums[k] = nums[i]; // Place the valid element at the front
+                k++;               // Increment the count/insertion pointer
+            }
+        }
+        
+        return k; // k is now the length of the array without 'val'
     }
 };
