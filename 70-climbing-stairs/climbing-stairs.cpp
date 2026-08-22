0ba32bff5eb685561d85da8d53a048f7b357b362
Sarthak Agarwal <163162441+sarthakagarwalco@users.noreply.github.com>
2026-08-22 22:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.96%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climb_stairs(vector<int> &dp, int n){
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return climb_stairs(dp, n);
+    }
+};
